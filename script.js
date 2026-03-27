051db3f7c3de9b4f5a365823ee651aff98ecdf3c
Trevor Tamp <trevor.tamp@tptlive.ee>
2026-03-27 11:03:05 +0200

    awawawaw

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 let tasks = [];
 
 function displayTasks() {
-	let html = "";
-	for (let i = 0; i < tasks.lenght; i++) {
-		html += "<li>" + tasks[i] +
-		"<button onclick='removeTask(" + i + ") '>x</button></li>";
-	}
-	document.getElementById("list").innerHTML = html;
+  let html = "";
+  for (let i = 0; i < tasks.length; i++) {
+    html += "<li>" + tasks[i] +
+      "<button onclick='removeTask(" + i + ")'>x</button></li>";
+  }
+  document.getElementById("list").innerHTML = html;
 }
+
 function addTask() {
   let taskInput = document.getElementById("task");
   let text = taskInput.value;
-  if (text === "") {
-    return;
-  }
+  if (text === "") return;
+
   tasks.push(text);
   taskInput.value = "";
-  saveTasks();
   displayTasks();
 }
+
 function removeTask(i) {
   tasks.splice(i, 1);
-  saveTasks();
+  displayTasks();
+}
+
+function clearAll() {
+  tasks = [];
   displayTasks();
 }
